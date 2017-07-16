4a42adf451d9187a33e9f2c87debdf554c2d2a7c
evgeni.chasnovski <evgeni.chasnovski@gmail.com>
2017-07-16 20:24:44 +0300

    Add documentation about supported functions.
    Update help page about package.

--- R/keyholder-package.R
+++ R/keyholder-package.R
@@ -7,8 +7,21 @@
 #' - hide columns for convenient use of [dplyr][dplyr::scoped]'s *_if scoped
 #'   variants of verbs.
 #'
-#' To learn more about `keyholder`, start with the vignette:
-#' browseVignettes(package = "keyholder")
+#' To learn more about `keyholder`:
+#' - Browse vignettes with `browseVignettes(package = "keyholder")`.
+#' - Look how to [set keys][keys-set].
+#' - Look at the list of [supported functions][keyholder-supported-funs].
 #'
 #' @import dplyr
 "_PACKAGE"
+
+
+#' Supported functions
+#'
+#' `keyholder` supports the following functions:
+#' - Base subsetting with \link{[}.
+#' - `dplyr` [one table verbs][keyed-df-one-tbl].
+#' - `dplyr` [two table verbs][keyed-df-two-tbl].
+#'
+#' @name keyholder-supported-funs
+NULL
